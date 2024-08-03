60aa4e3f568b29b8d328e7847b5c4cf1201a1b0f
이민규 <lmg386411@gmail.com>
2024-08-03 19:26:20 +0900

    SWEA D3 1206번 View

--- SWEA/D3/1206번 View.py
+++ SWEA/D3/1206번 View.py
@@ -7,13 +7,17 @@ for tc in range(1, T+1):
     lst = list(map(int, input().split()))
     cnt = 0
     for i in range(2, N-2):
+        # 우선 첫번째 층이 최대라고 가정하고
         mx = lst[i-2]
-        for j in range(i-1, i+3):
-            if i == j:
+        # 첫번째 층 제외하고 나머지 반복문 순회하면서
+        for si in range(i-1, i+3):
+            # 자신인 경우 예외처리
+            if i == si:
                 continue
-            else:
-                if lst[j] > mx:
-                    mx = lst[j]
+            # 자신을 제외한 최대의 층 수 계산
+            if lst[si] > mx:
+                mx = lst[si]
+        # 우리 집보다 자신을 제외한 최대의 층의 차이를 더하기
         if lst[i] >= mx:
             cnt += lst[i] - mx
     print(f"#{tc} {cnt}")
